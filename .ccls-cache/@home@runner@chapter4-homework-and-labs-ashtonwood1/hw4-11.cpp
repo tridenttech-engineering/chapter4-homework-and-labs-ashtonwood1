aec5c3df69fad1e04bc3473295ea0d1884ae966d
ashtonwood1 <ashtonhughwood@outlook.com>
2024-09-26 20:51:30 +0000

    HW4-11
    HW4-11

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-ashtonwood1/hw4-11.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-ashtonwood1/hw4-11.cpp
@@ -5,7 +5,24 @@
 using namespace std;
 
 int main() {
-  
+double beginning_balance;
+double total_deposits;
+double total_withdrawals;
+double end_balance;
+
+cout << "Beginning Balance:";
+cin >> beginning_balance;
+
+cout << "Total Deposits:";
+cin >> total_deposits;
+
+cout << "Total Withdrawals:";
+cin >> total_withdrawals;
+
+
+end_balance = beginning_balance + total_deposits - total_withdrawals;
+
+  cout << "End of Month Balance: " <<	end_balance << endl;
 
   return 0;
 } // end of main function
