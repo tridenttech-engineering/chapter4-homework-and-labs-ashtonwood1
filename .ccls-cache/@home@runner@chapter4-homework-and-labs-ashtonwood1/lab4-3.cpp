19ae420fc51efdb29fa8a31d03ffdbf3347913a3
ashtonwood1 <ashtonhughwood@outlook.com>
2024-09-26 20:16:45 +0000

    lab 4-3
    lab4-3

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-ashtonwood1/lab4-3.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-ashtonwood1/lab4-3.cpp
@@ -7,14 +7,15 @@ using namespace std;
 int main()
 {
   //declare named constant and variables
-  const double COMM_RATE = 0.1;
+  double COMM_RATE = 0.0;
   double sales = 0.0;
   double commission = 0.0;
 
   //enter input item
   cout << "Sales amount: ";
   cin >> sales;
-
+  cout << "Commission Rate: ";
+  cin >> COMM_RATE;
   //calculate and display the commision
   commission = sales * COMM_RATE;
   cout << "Commission: $" << commission << endl;
